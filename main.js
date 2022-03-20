d09c855074e19c51ea7151588357b8b9873c6ee9
SAM <75918417+SAM-dev-pixel@users.noreply.github.com>
2022-03-20 21:33:59 +0700

    Adding animations

--- main.js
+++ main.js
@@ -22,3 +22,45 @@
   });
   
 })(document.querySelector('.subscribe-btn'));
+
+// animations
+((readyText)=> {
+  
+  const arrStr = readyText.textContent.split('');
+  readyText.textContent = '';
+  arrStr.forEach(str => readyText.innerHTML += `<span class="ready-str">${str}</span>`);
+  
+})(document.querySelector('.ready-txt'));
+
+const animations =()=> {
+  
+  const heroImage = document.querySelector('.hero-img');
+  const textsP = document.querySelectorAll('.text');
+  const articleImages = document.querySelectorAll('article img');
+  const readyStr = document.querySelectorAll('.ready-str');
+  const footLinks = document.querySelectorAll('.foot-link');
+  
+  const screenHeight = window.innerHeight / 1.2;
+  
+  (heroImage.getBoundingClientRect().top < screenHeight) ? heroImage.classList.add('active') : heroImage.classList.remove('active');
+  textsP.forEach(text => (text.getBoundingClientRect().top < screenHeight) ? text.classList.add('active') : text.classList.remove('active'));
+  articleImages.forEach(img => (img.getBoundingClientRect().top < screenHeight) ? img.classList.add('active') : img.classList.remove('active'));
+  readyStr.forEach((str, i) => {
+    str.style.transition = `.3s ${0.025 * i}s`;
+    (str.getBoundingClientRect().top < screenHeight) ? str.style.opacity = 1 : str.style.opacity = 0;
+  });
+  footLinks.forEach(link => (link.getBoundingClientRect().top / 1.15< screenHeight) ? link.classList.add('active') : link.classList.remove('active'));
+  
+}
+window.addEventListener('scroll', animations);
+
+((introTitle)=> {
+  
+  const arrStr = introTitle.textContent.split('');
+  introTitle.textContent = '';
+  arrStr.forEach(str => introTitle.innerHTML += `<span class='intro-str'>${str}</span>`);
+  const introTitleSpans = document.querySelectorAll('.intro-str');
+  introTitleSpans.forEach((span, i) => span.style.transition = `.3s ${0.025 * i}s`);
+  setTimeout(()=> introTitleSpans.forEach(span => span.style.opacity = 1 ), 250)
+  
+})(document.querySelector('.intro-title'));
